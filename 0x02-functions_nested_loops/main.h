7edc59125d07b4bcb89750a7e43197a71cabd616
yassineelhafyane <yassineelhafyane99@gmail.com>
2023-06-20 09:51:45 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,19 @@
-#ifndef main_h
-#define main_h
+/**
+*File:main.h
+*Author:Yassine Elhafyane
+*Desc:header file containing functions declaration
+*/
 
-
-	int _putchar(char);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
-	int _islower(int c);
-	int _isalpha(int c);
-	int print_sign(int n);
-	int _abs(int);
-	int print_last_digit(int);
-	void jack_bauer(void);
-	void times_table(void);
-	int add(int, int);
-	void print_to_98(int n);
-
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
