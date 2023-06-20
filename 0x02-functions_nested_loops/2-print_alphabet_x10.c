24dca27d7ba5a60ad7c07860ecd2e0156eb73c50
yassineelhafyane <yassineelhafyane99@gmail.com>
2023-06-20 10:59:14 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
+ /**
+ * print_alphabet_x10 - write the the lower-case alphabet on a line.
+ *
+ */
+void print_alphabet_x10(void)
 
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
 	void print_alphabet_x10(void)
 	{
 		char ch;
