9864c6a857996908eefae384af8ad0aac2c00e62
yassineelhafyane <yassineelhafyane99@gmail.com>
2023-06-22 17:45:11 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,15 @@
 * main - Test function for positive or negative
 * Return: 0
 */
+
 int main(void)
 {
-int i;
 
-i = 0;
-positive_or_negative(i);
+	int i;
 
-return (0);
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
 }
